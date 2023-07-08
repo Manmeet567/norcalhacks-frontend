183025a96ec740eed6fdfa51cc9c2977a8c04cbd
Manmeet567 <manmeet567singh@gmail.com>
2023-07-08 20:05:01 +0530

    added responsiveness in navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,20 @@
 import './Navbar.css'
 import logo from '../../assets/logo.png'
 import { Link } from 'react-router-dom'
+import {GiHamburgerMenu} from 'react-icons/gi'
+import { useState } from 'react'
+import { Drawer,Box, Stack } from '@mui/material'
 
 function Navbar(props) {
 
   const items = props.items
 
+  const [open,setOpen] = useState(false)
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
-    <div className="navbar" style={{width:"100%", height:"70px", backgroundColor:"#398378", display:"flex", justifyContent:"center", boxShadow:"inset rgba(0, 0, 0, 0.2) 0px -20px 20px 0px"}}>
+    <div className="navbar" style={{position:"fixed", width:"100%", height:"70px", backgroundColor:"#398378", display:"flex", justifyContent:"center", boxShadow:"inset rgba(0, 0, 0, 0.2) 0px -20px 20px 0px, rgba(0, 0, 0, 0.2) 0px 20px 20px 0px"}}>
         <div className="inner-content" >
             <div className="logo" style={{display:"flex", alignItems:"end"}}>
               <img src={logo} alt="logo" width="60px" />
@@ -16,16 +23,27 @@ function Navbar(props) {
 
             <div className="menu-items">
               {items?.map((item) => (
-                <div className="item" key={item}>
+                <div className="item" key={item.name}>
                   <Link to={item.link} style={{color:"white", textDecoration:'none', fontSize:"20px", margin:"0 20px" }}>{item.name}</Link>
                 </div>
               ))}
 
-              {/* <div className="item">
-                <Link to='' style={{color:"white", textDecoration:'none', fontSize:"20px", margin:"0 20px"}}>Create Events</Link>
-              </div> */}
+              <GiHamburgerMenu className='menu-btn' onClick={handleOpen}/>
             </div>
         </div>
+
+        <Drawer open={open} anchor='right' onClose={handleClose}>
+            <div className="menu" style={{width:"300px", height:"100vh", backgroundColor:"#398378", display:"flex", justifyContent:"center"}}>
+              <Box sx={{width:"200px", height:"500px", mt:"80px"}}>
+                <Stack spacing={5} sx={{color:"white"}}>
+                  {items?.map((item) => (
+                    <Link key={item.name} to={item.link} style={{textDecoration:'none', color:"white"}}><h3>{item.name}</h3></Link>
+                  ))}
+                </Stack>
+              </Box>
+            </div>
+        </Drawer>
+
     </div>
   )
 }
