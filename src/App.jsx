3b816a968737716d7a4f7f0015b469db05cd890e
Manmeet567 <manmeet567singh@gmail.com>
2023-07-08 19:13:18 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import './App.css'
+import Home from './pages/Home/Home'
+import Login from './pages/Login/Login'
+import Signup from './pages/Signup/Signup'
+import UserHomePage from './pages/UserHomePage/UserHomePage'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="pages">
+        <Routes>
+
+          <Route path='/' element={<Home />} />
+
+          <Route path='/login' element={<Login />} />
+
+          <Route path='/signup' element={<Signup />} />
+
+          <Route path='/home' element={<UserHomePage />} />
+
+        </Routes>
+      </div>  
+    </BrowserRouter>
+  )
+}
+
+export default App
