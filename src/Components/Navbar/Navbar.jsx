ffb5fc680dbe102f878e25b5769294922e2e4aa7
Manmeet567 <manmeet567singh@gmail.com>
2023-07-10 15:10:15 +0530

    Added userProfile icon

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,34 +3,61 @@ import logo from '../../assets/logo.png'
 import { Link } from 'react-router-dom'
 import {GiHamburgerMenu} from 'react-icons/gi'
 import { useState } from 'react'
-import { Drawer,Box, Stack } from '@mui/material'
+import { Drawer,Box, Stack, Menu, MenuItem } from '@mui/material'
 import {SlClose} from 'react-icons/sl'
+import {FaUserCircle} from 'react-icons/fa'
 
 function Navbar(props) {
 
   const items = props.items
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const openMenu = Boolean(anchorEl);
+  const handleClickMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleCloseMenu = () => {
+    setAnchorEl(null);
+  };
 
   const [open,setOpen] = useState(false)
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
   return (
-    <div className="navbar" style={{position:"fixed", width:"100%", height:"70px", backgroundColor:"#398378", display:"flex", justifyContent:"center", boxShadow:"inset rgba(0, 0, 0, 0.2) 0px -20px 20px 0px, rgba(0, 0, 0, 0.2) 0px 20px 20px 0px"}}>
+    <div className="navbar" style={{position:"fixed", zIndex:"999", width:"100%", height:"70px", backgroundColor:"#398378", display:"flex", justifyContent:"center", boxShadow:"inset rgba(0, 0, 0, 0.2) 0px -20px 20px 0px, rgba(0, 0, 0, 0.2) 0px 20px 20px 0px"}}>
         <div className="inner-content" >
             <div className="logo" style={{display:"flex", alignItems:"end"}}>
               <img src={logo} alt="logo" width="60px" />
               <h1 style={{color:"white", marginBottom:"5px"}}>Taurite</h1>
             </div>
 
+            <Stack direction="row" spacing={3} sx={{alignItems:"center"}}>
             <div className="menu-items">
               {items?.map((item) => (
                 <div className="item" key={item.name}>
                   <Link to={item.link} style={{color:"white", textDecoration:'none', fontSize:"20px", margin:"0 20px" }}>{item.name}</Link>
                 </div>
               ))}
-
               <GiHamburgerMenu className='menu-btn' onClick={handleOpen}/>
             </div>
+            <div className="userAcc" title='Profile' style={{color:"white", fontSize:"30px", cursor:"pointer"}}>
+                <FaUserCircle id='user' aria-controls={openMenu ? 'basic-menu' : undefined} aria-haspopup="true" aria-expanded={openMenu ? 'true' : undefined} onClick={handleClickMenu}/>
+
+                <Menu id="basic-menu"
+                  anchorEl={anchorEl}
+                  open={openMenu}
+                  onClose={handleCloseMenu}
+                  MenuListProps={{
+                    'aria-labelledby': 'user',
+                  }}
+                >
+                  <MenuItem onClick={handleCloseMenu}>Profile</MenuItem>
+                  <MenuItem onClick={handleCloseMenu}>My account</MenuItem>
+                  <MenuItem onClick={handleCloseMenu}>Logout</MenuItem>
+                </Menu>
+            </div>
+            </Stack>
         </div>
 
         <Drawer open={open} anchor='right' onClose={handleClose}>
