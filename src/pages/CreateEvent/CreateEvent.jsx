2c809ce6b127e1b9e0e111e29942876f7dd02139
Manmeet567 <manmeet567singh@gmail.com>
2023-07-11 16:04:43 +0530

    created form

--- src/pages/CreateEvent/CreateEvent.jsx
+++ src/pages/CreateEvent/CreateEvent.jsx
@@ -28,7 +28,11 @@ function CreateEvent() {
             <h2 style={{color:"#398378", marginTop:"20px"}}>Create Event:</h2>
 
             <div className="ce-form" style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
-               
+                <input type="text" name="title" id="title" placeholder="Event Title" />
+                <textarea placeholder="Event Description" name="desc" />
+                <input type="text" name="location" placeholder="Event location" />
+                <input type="time" name="time" />
+                <button >Submit</button>
             </div>
 
         </div>
