c7d94c0cba220e9e62cea97e6d8bc719ddd88be1
Manmeet567 <manmeet567singh@gmail.com>
2023-07-12 09:51:05 +0530

    created hooks for signup login and logout

---
+++ src/hooks/useSignup.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { useAuthContext } from "./useAuthContext";
+
+export const useSignup = () => {
+    const [error, setError] = useState(null)
+    const [loading,setLoading] = useState(null)
+    const {dispatch} = useAuthContext()
+
+
+    const signup = async (username, password, rememberme) => {
+        setLoading(true)
+        setError(null)
+
+        const response = await fetch('http://localhost:4000/api/user/signup', {
+            method:"POST",
+            headers:{"Content-Type":"application/json"},
+            body:JSON.stringify({username, password})
+        })
+
+        const json = await response.json()
+
+        if(!response.ok) {
+            setLoading(false);
+            setError(json.error)
+        }
+
+        if(response.ok){
+            if(rememberme){
+                localStorage.setItem('user', JSON.stringify(json))
+            }
+            dispatch({type:'LOGIN', payload:json})
+            setLoading(false)
+        }
+    }
+
+    return {signup, loading, error}
+}
