de698179f796107a3b13e0864fdc18bc8f9fb884
Manmeet567 <manmeet567singh@gmail.com>
2023-07-16 10:25:32 +0530

    limited the routes for authenticated users

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,24 @@ import Login from './pages/Login/Login'
 import Signup from './pages/Signup/Signup'
 import UserHomePage from './pages/UserHomePage/UserHomePage'
 import CreateEvent from './pages/CreateEvent/CreateEvent'
+import { useAuthContext } from './hooks/useAuthContext'
 
 function App() {
+
+  const { user } = useAuthContext()
+
   return (
     <BrowserRouter>
       <div className="pages">
         <Routes>
 
-          <Route path='/' element={<Home />} />
+          <Route path='/' element={user ? <Navigate to='/home' /> : <Home />} />
 
-          <Route path='/login' element={<Login />} />
+          <Route path='/login' element={!user ? <Login/> : <Navigate to='/home' />} />
 
-          <Route path='/signup' element={<Signup />} />
+          <Route path='/signup' element={!user ? <Signup/> : <Navigate to='/home' />} />
 
-          <Route path='/home' element={<UserHomePage />} />
+          <Route path='/home' element={!user ? <Navigate to='/' /> : <UserHomePage />} />
 
           <Route path='/createEvent' element={<CreateEvent />} />
 
