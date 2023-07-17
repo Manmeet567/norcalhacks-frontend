3a7210cee05904c4b46e092f6bdccb3096604c6c
Manmeet567 <manmeet567singh@gmail.com>
2023-07-17 18:25:33 +0530

    created explore files folder

---
+++ src/pages/Explore/Explore.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react'
+import Navbar from '../../Components/Navbar/Navbar'
+import ExploreFeed from '../../Components/ExploreFeed/ExploreFeed'
+
+function Explore() {
+
+    const [countryList, setCountryList] = useState([]);
+
+    const Items = [
+        {
+            name:"Create Event",
+            link:"/createEvent"
+        },
+        {
+            name:"Share Post",
+            link:"/newPost"
+        },
+        {
+            name:"Explore",
+            link:"/explore"
+        }
+    ]
+
+    useEffect(() => {
+        const getCountries = async () =>{
+            const response = await fetch('https://restcountries.com/v3.1/all')
+
+            const json = await response.json()
+            if(!response.ok) {
+                setCountryList([])
+            }
+
+            if(response.ok) {
+                setCountryList(json)
+            }
+        }
+
+        getCountries();
+    }, [])
+
+  return (
+    <div className="explore">
+        <Navbar items = {Items}/>
+        
+        <ExploreFeed countries={countryList} />
+
+    </div>
+  )
+}
+
+export default Explore
