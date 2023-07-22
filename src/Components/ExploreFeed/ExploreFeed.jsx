dd74111687f8e7218233193c0d8a6972a671b56e
Manmeet567 <manmeet567singh@gmail.com>
2023-07-22 16:28:57 +0530

    created card component for events

--- src/Components/ExploreFeed/ExploreFeed.jsx
+++ src/Components/ExploreFeed/ExploreFeed.jsx
@@ -11,6 +11,8 @@ function ExploreFeed({ countries }) {
     setCountryNames(names.sort())
   }, [countries])
 
+  const string1 = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, dolorum aspernatur ipsum sit architecto veniam illo repellat?"
+
   return (
     <div className="exploreFeed" style={{ width: "100%", minHeight: "100vh", maxHeight: "100%", paddingTop: "70px", display: "flex", backgroundColor: "#66f1dc5d", justifyContent: "center" }}>
       <div className="inner-content-ef" >
@@ -35,7 +37,24 @@ function ExploreFeed({ countries }) {
 
         <div className="event-feed" >
           <h2 className="heading-ef" style={{color:"#398378"}}>Ongoing Events:</h2>
-          {/* Event feed content */}
+            <div className="events-section">
+
+                  <div className="event-card">
+                    <h2>Event-Title</h2>
+                    <div className="date-time">
+                      <p><b>Date - </b>12-July-2023</p>
+                      <p><b>12:30pm - 3:00pm</b></p>
+                    </div>
+                    <p style={{marginBottom:"8px"}}><b>Location - Hawaii</b></p>
+                    <p style={{marginBottom:"8px"}}><b>Created By - </b>Hiro</p>
+                    <h4>Description - </h4>
+                    <p style={{textAlign:"justify"}}>{string1}</p>
+
+                    <button style={{margin:"14px auto 0", width:"100px", borderRadius:"5px", border:"none", padding:"10px 0", backgroundColor:"#398378", color:"white", cursor:"pointer"}}>Learn More</button>
+                  </div>
+
+                  
+            </div>
         </div>
       </div>
     </div>
